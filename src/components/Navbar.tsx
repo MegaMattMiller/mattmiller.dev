fab90fafbd924dc3f5d9b4e47a8ab236071fbafa
Matt Miller <matt@mattmiller.dev>
2022-08-09 18:04:37 -0500

    fix: fix tailwind classes for navbar buttons

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -8,8 +8,8 @@ const Navbar:FC = () => {
       <h1 className="pr-0">Matt Miller | Programmer</h1>
       <h2 className='pl-5'><a href="mailto:matt@mattmiller.dev">matt@mattmiller.dev</a></h2>
       <a className="md:text-[4vw] lg:text-[2vw] text-xl ml-auto" href="https://github.com/MegaMattMiller" title='github link'><FaGithubSquare /></a>
-      <a className="md:text-[4vw] lg:text-[2vw] tex0t-xl ml-0" href="https://dev.to/megamattmiller" title='dev.to link'><FaDev /></a>
-      <a className="md:text-[4vw] lg:text-[2vw] tex0t-xl ml-0" href="mailto:matt@mattmiller.dev" title='email link'><FaEnvelopeSquare /></a>
+      <a className="md:text-[4vw] lg:text-[2vw] text-xl ml-0" href="https://dev.to/megamattmiller" title='dev.to link'><FaDev /></a>
+      <a className="md:text-[4vw] lg:text-[2vw] text-xl ml-0" href="mailto:matt@mattmiller.dev" title='email link'><FaEnvelopeSquare /></a>
     </div>
   )
 }
