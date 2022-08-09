53848ed434593bcf79ba2533c01b491fe45e5d3a
Matt Miller <matt@mattmiller.dev>
2022-08-09 17:57:33 -0500

    feat: add title to anchors for a11y

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,9 +7,9 @@ const Navbar = () => {
     <div className="navbar bg-neutral text-neutral-content w-full flex flex-row justify-start">
       <h1 className="pr-0">Matt Miller | Programmer</h1>
       <h2 className='pl-5'><a href="mailto:matt@mattmiller.dev">matt@mattmiller.dev</a></h2>
-      <a className="md:text-[4vw] lg:text-[2vw] text-xl ml-auto" href="https://github.com/MegaMattMiller"><FaGithubSquare /></a>
-      <a className="md:text-[4vw] lg:text-[2vw] tex0t-xl ml-0" href="https://dev.to/megamattmiller"><FaDev /></a>
-      <a className="md:text-[4vw] lg:text-[2vw] tex0t-xl ml-0" href="mailto:matt@mattmiller.dev"><FaEnvelopeSquare /></a>
+      <a className="md:text-[4vw] lg:text-[2vw] text-xl ml-auto" href="https://github.com/MegaMattMiller" title='github link'><FaGithubSquare /></a>
+      <a className="md:text-[4vw] lg:text-[2vw] tex0t-xl ml-0" href="https://dev.to/megamattmiller" title='dev.to link'><FaDev /></a>
+      <a className="md:text-[4vw] lg:text-[2vw] tex0t-xl ml-0" href="mailto:matt@mattmiller.dev" title='email link'><FaEnvelopeSquare /></a>
     </div>
   )
 }
